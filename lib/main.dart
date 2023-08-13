72ef7f5fc9f79761846b48d66454a763dcf0cccd
kimyejin <7602kyj@e-mirim.hs.kr>
2023-08-13 20:21:13 +0900

    #2 themes 클래스 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktokclonecoding/sign_up/screen/sign_up_screen.dart';
-import 'package:tiktokclonecoding/constants/sizes.dart';
+import 'package:tiktokclonecoding/styles/themes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -13,21 +13,9 @@ class TikTokApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TikTok App',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFE9435A),
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size18,
-            fontWeight: FontWeight.w600,
-          )
-        ),
-      ),
+      theme: Themes.themeData,
       home: SignUpScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
